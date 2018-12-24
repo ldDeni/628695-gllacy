62efca0319831c8f89dfa880c18c1a7da768e97a
ldDen <lepekhin_denis@mail.ru>
2018-12-24 05:01:25 +0500

    Сделал фильтр товаров, кроме ренжа

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var openFeedback = document.querySelector(".bnt-contacts"); // Кнопка об
 var windowFeedback = document.querySelector(".feedback-window"); // Окно формы.
 var closeFeedback = windowFeedback.querySelector(".button-close"); // Кнопка закрытия.
 var name = windowFeedback.querySelector(".enter-name");
+var mail = windowFeedback.querySelector(".enter-mail");
 var overlay = document.querySelector(".overlay");
 
 openFeedback.addEventListener("click", function (evt) {
@@ -16,4 +17,27 @@ closeFeedback.addEventListener("click", function (evt) {
     windowFeedback.classList.remove("feedback-open");   // закрывает форму обратной связи.
     overlay.classList.remove("overlay-on");
 });
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (windowFeedback.classList.contains("feedback-open")) {
+        windowFeedback.classList.remove("feedback-open");
+        overlay.classList.remove("overlay-on");
+      }
+    }
+  });
 
+if (!name.value || mail.value) {
+
+}
+
+form.addEventListener("submit", function (evt) {
+    if (!name.value || mail.value) {
+      evt.preventDefault();
+      windowFeedbac.classList.add("feedback-error");
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("login", login.value);
+      }
+    }
+  });
